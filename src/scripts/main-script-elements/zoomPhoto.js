9c2765252633b6ff58e119f71704621776e87e4e
BossNumber1 <dimaang23@gmail.com>
2023-09-12 20:22:22 +0300

    up

--- src/scripts/main-script-elements/zoomPhoto.js
+++ src/scripts/main-script-elements/zoomPhoto.js
@@ -1,17 +1,18 @@
 import { pathToPhoto } from "./commonFunctions/commonFunctions";
 import { allPhoto, modalPhotoBlock, video } from "./gettedElems";
-import { numberPhotosPerPage, firstVisiblePhotoNumber } from "./store/commonData";
+import { firstVisiblePhotoNumber } from "./store/commonData";
 
 export function showEnlargedPhoto() {
   Array.from(allPhoto()).forEach((image, index) => {
     image.addEventListener("click", function () {
+
       // При клике на фотографию устанавливаем фото в модальном окне
       let zoomPhotoCardContent = document.getElementsByClassName("zoom-photo-area__main-content")[0];
       zoomPhotoCardContent.style.backgroundImage = pathToPhoto(index + firstVisiblePhotoNumber + 1);
-      // alert("index = " + index);
-      // alert("numberPhotosPerPage = " + numberPhotosPerPage);
+
       // Затем делаем видимым модальное окно
       modalPhotoBlock().style.display = "flex";
+
       // Скрываем видео, чтобы при показе модального окна с фотографией оно не было видно пользователю
       video().style.display = "none";
     });
@@ -19,6 +20,7 @@ export function showEnlargedPhoto() {
 }
 
 document.getElementsByClassName("zoom-photo-area__close-icon")[0].addEventListener("click", function() {
+  
   // Делаем невидимым модальное окно с фотографией и видимым видео
   modalPhotoBlock().style.display = "none";
   video().style.display = "block";
